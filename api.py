6030d1861fbdb2dae487400cb9545a6ce6569dda
Michael Scheffenacker <michael.scheffenacker@gmail.com>
2018-11-12 03:28:47 +0100

    API: finally added /grab_and_save; tested manually (curl)

--- api.py
+++ api.py
@@ -2,14 +2,15 @@ from contextlib import contextmanager
 from flask import Flask
 from flask_restful import Resource, Api, reqparse, abort
 
-from db import DB
+import db
 import sanitizers
+from requests_record import RequestRecord
+from oer_requests import get_rate
 
 
 app = Flask(__name__)
 api = Api(app)
 
-db = DB()
 
 @contextmanager
 def sanitizer_scope():
@@ -19,28 +20,56 @@ def sanitizer_scope():
         abort(400, message=str(err))
 
 
+parser = reqparse.RequestParser()
+parser.add_argument(
+    'amount',
+    type=float,
+    help="'amount' has to be a float that represents the amount in the given currency"
+)
+parser.add_argument(
+    'currency',
+    type=str,
+    help="'currency' has to be a string with exactly three letters [A-Z] e.g. EUR"
+)
+
+
 class GrabAndSave(Resource):
     def post(self):
-        return {'hello': 'world'}
+        args = parser.parse_args(strict=True)
+        with sanitizer_scope():
+            amount = sanitizers.amount(args['amount'])
+            currency = sanitizers.currency(args['currency'])
+            exchange_rate = get_rate(currency)
+
+        calculated_amount = amount / exchange_rate
+
+        request = RequestRecord(
+            currency=currency,
+            requested_amount=amount,
+            exchange_rate=exchange_rate,
+            calculated_amount_USD=calculated_amount
+        )
+
+        db.insert_request(request)
 
 
 class Last(Resource):
     def get(self):
-        return db.get_last_requests(number=1, currency=None)
+        return db.select_last_requests(number=1, currency=None)
 
 
 class LastCurrency(Resource):
     def get(self, currency):
         with sanitizer_scope():
             currency = sanitizers.currency(currency)
-        return db.get_last_requests(number=1, currency=currency)
+        return db.select_last_requests(number=1, currency=currency)
 
 
 class LastN(Resource):
     def get(self, number):
         with sanitizer_scope():
             number = sanitizers.number(number)
-        return db.get_last_requests(number, currency=None)
+        return db.select_last_requests(number, currency=None)
 
 
 class LastNCurrency(Resource):
@@ -48,7 +77,7 @@ class LastNCurrency(Resource):
         with sanitizer_scope():
             number = sanitizers.number(number)
             currency = sanitizers.currency(currency)
-        return db.get_last_requests(number, currency)
+        return db.select_last_requests(number, currency)
 
 
 api.add_resource(GrabAndSave, '/grab_and_save')
